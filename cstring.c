4b80ebbcc0a8491583cd4f6dd3eeb41cf5d38a01
EC2 Default User <ec2-user@ip-172-31-38-82.us-east-2.compute.internal>
2018-08-31 16:59:27 +0000

    added cpp

--- cstring.c
+++ cstring.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "string.h"
 int main(void) {
   char text[128] = "Andy is the greatest";
   //Old fashioned C strings
@@ -6,7 +7,7 @@ int main(void) {
   // char text[128] declares an array of 'char's and = "..." initializes the values stored in that array
   // after the "t" in greatest there will be stored a null byte (00000000) to indicate the end of the character array
 
-  printf("%s\n", text);
+  printf("%i\n", strlen(text));
   //The format flag %s will print out the entire character array as text
   //this is the old fashioned version of a "string"
   return 0;
